f185e4cf040da694c050294df2fe781a6fb84d62
rrahman <rashidur92@hotmail.com>
2022-11-25 17:09:22 +0000

    parsing arguments in detail

--- main/main.go
+++ main/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "lemin"
+import (
+	"fmt"
+	"lemin"
+)
 
 func main() {
-	lemin.ParseArgs()
+	numberofants, startingroom, endingroom, allrooms, roomLinks, outputError := lemin.ParseArgs()
+
+	fmt.Println("number of ants: ", numberofants)
+	fmt.Println("starting room: ", startingroom, "ending room: ", endingroom)
+	fmt.Println("all rooms: ", allrooms)
+	fmt.Println("room links: ", roomLinks)
+	fmt.Println("first room: ", allrooms[0])
+	fmt.Println("first room links fields: ", allrooms[0].LinksAsStrings, allrooms[0].LinksAsPointers)
+	fmt.Println("detected error: ", outputError)
 }
--- parseargs.go
+++ parseargs.go
@@ -1,6 +1,7 @@
 package lemin
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -8,17 +9,46 @@ import (
 )
 
 type Room struct {
-	Name   string
-	xcoord int
-	ycoord int
+	Name            string
+	Xcoord          int
+	Ycoord          int
+	LinksAsStrings  []string
+	LinksAsPointers []*Room
 }
 
-func ParseArgs() (numberofants int, startingroom Room, endingroom Room, allrooms []Room, roomLinks []string, outputError error) {
+// takes as input a slice of rooms and a slice of string that has the room links and populates the links field in the rooms
+func RoomLinkerStrings(unlinkedRooms []Room, roomLinks [][]string) []Room {
+	for roomLinkIndex := 0; roomLinkIndex < len(roomLinks); roomLinkIndex++ {
+		for roomIndex := 0; roomIndex < len(unlinkedRooms); roomIndex++ {
+			if unlinkedRooms[roomIndex].Name == roomLinks[roomLinkIndex][0] {
+				unlinkedRooms[roomIndex].LinksAsStrings = append(unlinkedRooms[roomIndex].LinksAsStrings, roomLinks[roomLinkIndex][1])
+			}
+		}
+	}
+	return unlinkedRooms
+}
+
+func RoomLinkerPointers(unlinkedRooms []Room, roomLinks [][]string) []Room {
+	for roomLinkIndex := 0; roomLinkIndex < len(roomLinks); roomLinkIndex++ {
+		for roomIndex := 0; roomIndex < len(unlinkedRooms); roomIndex++ {
+			if unlinkedRooms[roomIndex].Name == roomLinks[roomLinkIndex][0] {
+				for roomToLinkIndex := 0; roomToLinkIndex < len(unlinkedRooms); roomToLinkIndex++ {
+					if unlinkedRooms[roomToLinkIndex].Name == roomLinks[roomToLinkIndex][1] {
+						unlinkedRooms[roomIndex].LinksAsPointers = append(unlinkedRooms[roomIndex].LinksAsPointers, &unlinkedRooms[roomToLinkIndex])
+					}
+				}
+			}
+		}
+	}
+	return unlinkedRooms
+}
+
+func ParseArgs() (numberofants int, startingroom Room, endingroom Room, allrooms []Room, roomLinks [][]string, outputError error) {
 	filename := os.Args[1]
 
 	data, outputError := os.ReadFile(filename)
 	if outputError != nil {
-		return 0, Room{}, Room{}, []Room{}, []string{}, outputError
+		return 0, Room{}, Room{}, []Room{}, [][]string{}, outputError
 	}
 
 	instructions := strings.Split(string(data), "\n")
@@ -26,7 +56,7 @@ func ParseArgs() (numberofants int, startingroom Room, endingroom Room, allrooms
 	// first line should always be number of ants
 	numberofants, outputError = strconv.Atoi(instructions[0])
 	if outputError != nil {
-		return 0, Room{}, Room{}, []Room{}, []string{}, outputError
+		return 0, Room{}, Room{}, []Room{}, [][]string{}, outputError
 	}
 
 	for i := 1; i < len(instructions); i++ {
@@ -38,17 +68,17 @@ func ParseArgs() (numberofants int, startingroom Room, endingroom Room, allrooms
 		// splits line based on spaces to differentiate number of ants, room info, room links etc.
 		words := strings.Fields(line)
 
-		// only room info should be a line with 3 objects separated by spaces, first object is name second object is xcoord third object is ycoord
+		// only room info should be a line with 3 objects separated by spaces, first object is name second object is Xcoord third object is Ycoord
 		if len(words) == 3 {
 			currentRoom := Room{}
 			currentRoom.Name = words[0]
-			currentRoom.xcoord, outputError = strconv.Atoi(words[1])
+			currentRoom.Xcoord, outputError = strconv.Atoi(words[1])
 			if outputError != nil {
-				return 0, Room{}, Room{}, []Room{}, []string{}, outputError
+				return 0, Room{}, Room{}, []Room{}, [][]string{}, outputError
 			}
-			currentRoom.ycoord, outputError = strconv.Atoi(words[2])
+			currentRoom.Ycoord, outputError = strconv.Atoi(words[2])
 			if outputError != nil {
-				return 0, Room{}, Room{}, []Room{}, []string{}, outputError
+				return 0, Room{}, Room{}, []Room{}, [][]string{}, outputError
 			}
 
 			allrooms = append(allrooms, currentRoom)
@@ -64,9 +94,16 @@ func ParseArgs() (numberofants int, startingroom Room, endingroom Room, allrooms
 		// apart from number of ants on first line, only other time you should have only one object with no spaces is either room links or start or end tags
 		// maybe have more checks for other possibilities for incorrect formats to throw errors
 		if len(words) == 1 && line != "##start" && line != "##end" {
-			roomLinks = append(roomLinks, line)
+			links := strings.Split(line, "-")
+			if len(links) != 2 {
+				return 0, Room{}, Room{}, []Room{}, [][]string{}, errors.New("roomlink incorrect format")
+			}
+			roomLinks = append(roomLinks, links)
 		}
 	}
+
+	allrooms = RoomLinkerPointers(allrooms, roomLinks)
+	allrooms = RoomLinkerStrings(allrooms, roomLinks)
 
 	return numberofants, startingroom, endingroom, allrooms, roomLinks, nil
 }
