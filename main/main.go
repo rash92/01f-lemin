50a84521d1d54d2999132766e1284a95940c8978
Michael A <mikeade08l@GMAIL.COM>
2022-12-05 18:29:07 +0000

    michael's ant andler

--- main/main.go
+++ main/main.go
@@ -7,26 +7,33 @@ import (
 )
 
 func main() {
-	numberofants, startingRoom, endingRoom, allRooms, roomLinks, outputError := lemin.ParseArgs()
+	numberofants, startingRoom, endingRoom, allRooms, _, outputError := lemin.ParseArgs()
+	// _, startingRoom, endingRoom, allRooms, _, outputError := lemin.ParseArgs()
+
 	if outputError != nil {
 		fmt.Println(outputError)
 		log.Fatal("couldn't parse file")
 	}
 
-	fmt.Println("detected error: ", outputError)
-	fmt.Println("number of ants: ", numberofants)
-	fmt.Println("starting room: ", startingRoom, "ending room: ", endingRoom)
-	fmt.Println("all rooms: ", allRooms)
-	fmt.Println("room links: ", roomLinks)
+	// fmt.Println("detected error: ", outputError)
+	// fmt.Println("number of ants: ", numberofants)
+	// fmt.Println("starting room: ", startingRoom, "ending room: ", endingRoom)
+	// fmt.Println("all rooms: ", allRooms)
+	// fmt.Println("room links: ", roomLinks)
 
-	routes := [][]lemin.Room{}
-	allroutes := lemin.FindAllRoutes(startingRoom, endingRoom, allRooms, routes)
-	for i, route := range allroutes {
-		fmt.Println("route number: ", i)
-		for j, room := range route {
-			fmt.Println("room ", j, " in route: ", room.Name)
-		}
-	}
-	fmt.Println("ant paths: ")
-	lemin.AntHandler(allroutes, numberofants)
+	// routes := [][]lemin.Room{}
+	allroutes := lemin.FindAllRoutes(startingRoom, endingRoom, allRooms, [][]lemin.Room{})
+	// for i, route := range allroutes {
+	// 	fmt.Println("route number: ", i)
+	// 	for j, room := range route {
+	// 		fmt.Println("room ", j, " in route: ", room.Name)
+	// 	}
+	// }
+	// fmt.Println("ant paths: ")
+	AntHandler(allroutes, numberofants)
+
+}
+
+func AntHandler(routes [][]lemin.Room, numberofants int) {
+
 }
